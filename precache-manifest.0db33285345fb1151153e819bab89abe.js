324e01f5b1aee3e3c63a527a036e337cd6c86fdd
jahorinac <registracijae@gmail.com>
2020-06-10 13:08:11 +0200

    Updates

---
+++ precache-manifest.0db33285345fb1151153e819bab89abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669ebeb76ccac6c89df33f70b533d383",
+    "url": "/02-firebase-redux-react/index.html"
+  },
+  {
+    "revision": "a873e9e98f2b0aa52113",
+    "url": "/02-firebase-redux-react/static/css/main.1d194e01.chunk.css"
+  },
+  {
+    "revision": "e54f298ca803d8b52106",
+    "url": "/02-firebase-redux-react/static/js/2.87749186.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/02-firebase-redux-react/static/js/2.87749186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a873e9e98f2b0aa52113",
+    "url": "/02-firebase-redux-react/static/js/main.9a903899.chunk.js"
+  },
+  {
+    "revision": "a0384b65b7b9fa1545ed",
+    "url": "/02-firebase-redux-react/static/js/runtime-main.58413808.js"
+  }
+]);
